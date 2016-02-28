4c08a49f5f88c46e1741f11ed30fea6ffc9d800b
AlekGR <alek.grischenko@gmail.com>
2016-02-28 21:44:12 +0300

    Use /api urls instead /api_flashcards/api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 ApiFlashcards::Engine.routes.draw do
   root 'home#welcome'
 
-  namespace :api do
+  scope module: 'api' do
     namespace :v1 do
       get :cards, to: 'cards#index'
       post :cards, to: 'cards#create'
+
+      get :rewiew_card, to: 'review#review_card'
     end
   end
 end
