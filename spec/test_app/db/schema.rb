3fbd17d665b6f4ec729c0278f84f67d75fbf573b
AlekGR <alek.grischenko@gmail.com>
2016-03-01 10:08:54 +0300

    Add cards and blocks to test_app.

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228150000) do
+ActiveRecord::Schema.define(version: 20160229101618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blocks", force: :cascade do |t|
+    t.string  "title",   null: false
+    t.integer "user_id", null: false
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string  "original_text"
+    t.string  "translated_text"
+    t.date    "review_date"
+    t.integer "user_id",         null: false
+    t.integer "block_id",        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
